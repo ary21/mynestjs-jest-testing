035a0bab724518be9f4b96b39d2af5b6d7bda42e
ary21 <aryjamers.21@gmail.com>
2022-07-28 06:21:51 +0700

    simple prisma

---
+++ prisma/migrations/20220725213042_init/migration.sql
@@ -0,0 +1,18 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('USER', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "nik" TEXT,
+    "deviceToken" TEXT,
+    "role" "UserRole" NOT NULL DEFAULT 'USER',
+    "deleted" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
